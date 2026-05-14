ff5d95948380e753c8b4517bea76cae198b80b05
Will Ford <will.ford21@imperial.ac.uk>
2026-05-14 21:26:47 +0100

    feat: make strava button interactive

---
+++ link-pages/sports-pages/running.js
@@ -0,0 +1,5 @@
+let stravaButton = document.getElementById("strava-button");
+
+stravaButton.addEventListener("click", function() {
+    window.open('https://www.strava.com/athletes/116195932', '_blank');
+}, false)
