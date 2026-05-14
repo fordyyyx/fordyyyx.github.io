55687c65b0dfb583a4d1973a02a45031038a1c1b
Will Ford <will.ford21@imperial.ac.uk>
2026-05-14 21:54:56 +0100

    feat: make home button work

--- link-pages/sports-pages/running.js
+++ link-pages/sports-pages/running.js
@@ -1,5 +1,10 @@
 let stravaButton = document.getElementById("strava-button");
+let homeButton = document.getElementById("home-nav-button");
 
 stravaButton.addEventListener("click", function() {
     window.open('https://www.strava.com/athletes/116195932', '_blank');
 }, false)
+
+homeButton.addEventListener("click", function() {
+    location.href = '../../index.html';
+})
